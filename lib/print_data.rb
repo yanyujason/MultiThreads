1c53ad15d1955b6b66914fc3095b30cf5e21aa8b
Yu Yan <yu.yan@rea-group.com>
2014-03-24 23:02:53 +0800

    fix semphore

--- lib/print_data.rb
+++ lib/print_data.rb
@@ -11,23 +11,23 @@ data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 
 @thread_number.times do |x|
   thread = Thread.new do
-    while (data.length > 0) do
+    loop do
       semaphores.synchronize do
+        num = 0
         mutex.synchronize do
           num = data[0]
           data = data[1..-1]
-          print_data x, num
         end
-        sleep 1
+        print_data x, num if num != nil
       end
-      puts "Thread #{x} ended\n"
     end
   end
 end
 
 
 def print_data(thread, num)
+  sleep 1
   puts "Thread #{thread} printed out number #{num}\n"
 end
 
-sleep 20
+sleep 10
